d843e26b8008f9df7cb90a367d0a5c7b18e970f6
Costache Robert <73911181+mckn1ght@users.noreply.github.com>
2021-02-09 13:32:50 +0200

    -Implementation of quit button -Reworked the validation process -String formatter for First name and Last name

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
@@ -38,14 +39,13 @@ public class Controller {
     @FXML
     Button quitButton;
 
-    static boolean username, firstname, lastname, email, phonenumber, password, confirmpassword;
+    boolean username, firstname, lastname, email, phonenumber, password, confirmpassword;
 
     public void checkUsernameField(){
 
         Pattern specialCharacters = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
         Matcher matcherUsernameField = specialCharacters.matcher(usernameField.getText());
         username = true;
-
         if (matcherUsernameField.find()) {
             error.setVisible(true);
             error.setText("Username must contain only letters or numbers");
@@ -67,7 +67,6 @@ public class Controller {
             firstname = false;
         }
         else {
-
             resetTextField(firstNameField);
         }
     }
@@ -83,26 +82,24 @@ public class Controller {
             lastname = false;
         }
         else {
-
             resetTextField(lastNameField);
         }
     }
 
     public void checkEmailAdressField(){
         email = true;
-        //Pattern specialCharacters = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
-
         Pattern specialCharacters = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])", Pattern.CASE_INSENSITIVE);
         Matcher matcherEmailField = specialCharacters.matcher(emailField.getText());
-        if(!matcherEmailField.find()){
-
-            error.setVisible(true);
-            error.setText("That is not a valid email adress");
-            emailField.setStyle("-fx-background-color: red");
-            email = false;
-        }
-        else {
-
+        if(!emailField.getText().equals("")) {
+            if (!matcherEmailField.find()) {
+                error.setVisible(true);
+                error.setText("That is not a valid email adress");
+                emailField.setStyle("-fx-background-color: red");
+                email = false;
+            } else {
+                resetTextField(emailField);
+            }
+        }else {
             resetTextField(emailField);
         }
     }
@@ -111,7 +108,7 @@ public class Controller {
         phonenumber = true;
         Pattern onlyNumbers = Pattern.compile("[^0-9]");
         Matcher matcherPhoneNumberField = onlyNumbers.matcher(phoneNumberField.getText());
-        if((phoneNumberField.getText().length() < 5 && phoneNumberField.getText().length() > 0) || phoneNumberField.getText().length() > 15 || matcherPhoneNumberField.find()) {
+        if((phoneNumberField.getText().length() < 5 && phoneNumberField.getText().length() > 0) ||  phoneNumberField.getText().length() > 15 || matcherPhoneNumberField.find()) {
             error.setVisible(true);
             error.setText("That is not a valid phone number");
             phoneNumberField.setStyle("-fx-background-color: red");
@@ -122,100 +119,123 @@ public class Controller {
         }
     }
 
-    public void checkPasswordField() {
-        String specialChars = "~`!@#$%^&*()-_=+\\|[{]};:'\",<.>/?";
+    public boolean checkPasswordField() {
         password = true;
+        String specialChars = "~`!@#$%^&*()-_=+\\|[{]};:'\",<.>/?";
         char currentCharacter;
         boolean numberPresent = false;
         boolean upperCasePresent = false;
         boolean specialCharacterPresent = false;
-
-        for (int i = 0; i < passwordField.getText().length(); i++) {
-            currentCharacter = passwordField.getText().charAt(i);
-            if (Character.isDigit(currentCharacter)) {
-                numberPresent = true;
-            } else if (Character.isUpperCase(currentCharacter)) {
-                upperCasePresent = true;
-            } else if (specialChars.contains(String.valueOf(currentCharacter))) {
-                specialCharacterPresent = true;
+        if(!passwordField.getText().equals("")) {
+            for (int i = 0; i < passwordField.getText().length(); i++) {
+                currentCharacter = passwordField.getText().charAt(i);
+                if (Character.isDigit(currentCharacter)) {
+                    numberPresent = true;
+                } else if (Character.isUpperCase(currentCharacter)) {
+                    upperCasePresent = true;
+                } else if (specialChars.contains(String.valueOf(currentCharacter))) {
+                    specialCharacterPresent = true;
+                }
             }
-        }
 
-        if((passwordField.getText().length() > 0 && passwordField.getText().length() < 8) || passwordField.getText().length() > 20){
-            password = false;
-            error.setVisible(true);
-            error.setText("Password must be between 8 and 20 characters long");
-            passwordField.setStyle("-fx-background-color: red");
-        }
-        if(!numberPresent){
-            password =false;
-            error.setVisible(true);
-            error.setText("Password must contain at least 1 number");
-            passwordField.setStyle("-fx-background-color: red");
-            return;
-        }
-        if(!upperCasePresent){
-            password =false;
-            error.setVisible(true);
-            error.setText("Password must contain at least 1 Upper case");
-            passwordField.setStyle("-fx-background-color: red");
-            return;
-        }
-        if(!specialCharacterPresent) {
-            password =false;
-            error.setVisible(true);
-            error.setText("Password must contain at least 1 special character");
-            passwordField.setStyle("-fx-background-color: red");
-            return;
-        }
-        if(!confirmPasswordField.getText().equals(passwordField.getText())){
-            password = false;
-            error.setVisible(true);
-            error.setText("Passwords do not match");
-            confirmPasswordField.setStyle("-fx-background-color: red");
+            if ((passwordField.getText().length() > 0 && passwordField.getText().length() < 8) || passwordField.getText().length() > 20) {
+                error.setVisible(true);
+                error.setText("Password must be between 8 and 20 characters long");
+                passwordField.setStyle("-fx-background-color: red");
+                password = false;
+                return false;
+            }
+            if (!numberPresent) {
+                error.setVisible(true);
+                error.setText("Password must contain at least 1 number");
+                passwordField.setStyle("-fx-background-color: red");
+                password = false;
+                return false;
+            }
+            if (!upperCasePresent) {
+                error.setVisible(true);
+                error.setText("Password must contain at least 1 Upper case");
+                passwordField.setStyle("-fx-background-color: red");
+                password = false;
+                return false;
+            }
+            if (!specialCharacterPresent) {
+                error.setVisible(true);
+                error.setText("Password must contain at least 1 special character");
+                passwordField.setStyle("-fx-background-color: red");
+                password = false;
+                return false;
+            }
+            if (!confirmPasswordField.getText().equals(passwordField.getText())) {
+                error.setVisible(true);
+                error.setText("Passwords do not match");
+                confirmPasswordField.setStyle("-fx-background-color: red");
+                password = false;
+                return false;
+            } else {
+                resetPasswordField(passwordField, confirmPasswordField);
+            }
+            if (password) {
+                resetPasswordField(passwordField);
+                return true;
+            }
         }else{
-            resetPasswordField(passwordField, confirmPasswordField);
-        }
-
-        if(password){
-
             resetPasswordField(passwordField);
+            return false;
         }
+        return true;
     }
+
 
     public void checkConfirmPasswordField(){
         confirmpassword = true;
-        if(!confirmPasswordField.getText().equals(passwordField.getText())){
-            confirmpassword = false;
-            error.setVisible(true);
-            error.setText("Passwords do not match");
-            confirmPasswordField.setStyle("-fx-background-color: red");
-        }
-        else {
-
-            resetPasswordField(confirmPasswordField, passwordField);
-        }
+        if(!confirmPasswordField.getText().equals("")) {
+            if (!confirmPasswordField.getText().equals(passwordField.getText())) {
+                confirmpassword = false;
+                error.setVisible(true);
+                error.setText("Passwords do not match");
+                confirmPasswordField.setStyle("-fx-background-color: red");
+            } else if (password && checkPasswordField()) {
+                resetPasswordField(confirmPasswordField, passwordField);
+            }else checkPasswordField();
+        }else resetPasswordField(confirmPasswordField);
     }
 
     public void register() {
         if (usernameField.getText().isEmpty() || firstNameField.getText().isEmpty() || lastNameField.getText().isEmpty() || emailField.getText().isEmpty() || phoneNumberField.getText().isEmpty() ||
                 passwordField.getText().isEmpty() || confirmPasswordField.getText().isEmpty()){
-
             error.setVisible(true);
             error.setText("You must complete all the fields first");
-    }
-        if(username && firstname && lastname && email && phonenumber && password && confirmpassword) {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Succes!");
-            alert.setHeaderText(null);
-            alert.setContentText("Account created, you can now log in.");
-            alert.showAndWait();
-        }else{
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("ERROR!");
-            alert.setHeaderText(null);
-            alert.setContentText("Please correct all the issues first");
-            alert.showAndWait();
+        }else {
+            if (username && firstname && lastname && email && phonenumber && password && confirmpassword) {
+                //First name and Last name formatter
+                //firstname
+                String capitalLetter = firstNameField.getText();
+                StringBuilder sb = new StringBuilder(capitalLetter);
+                sb.setCharAt(0, Character.toUpperCase(sb.charAt(0)));
+                capitalLetter = sb.toString();
+                firstNameField.setText(capitalLetter);
+
+                //lastname
+                capitalLetter = lastNameField.getText();
+                sb = new StringBuilder(capitalLetter);
+                sb.setCharAt(0, Character.toUpperCase(sb.charAt(0)));
+                capitalLetter = sb.toString();
+                lastNameField.setText(capitalLetter);
+                //
+
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Succes!");
+                alert.setHeaderText(null);
+                alert.setContentText("Account created, you can now log in.");
+                alert.showAndWait();
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("ERROR!");
+                alert.setHeaderText(null);
+                alert.setContentText("Please correct all the issues first");
+                alert.showAndWait();
+            }
         }
     }
 
@@ -240,5 +260,9 @@ public class Controller {
             error.setVisible(false);
             pw.setStyle(null);
         }
+    }
+
+    public void quit(){
+        Platform.exit();
     }
 }
